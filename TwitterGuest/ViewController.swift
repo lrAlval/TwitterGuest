5e5047457e2557101a5c670d6930795dd30a35c5
asd <gg@imac.local>
2017-05-28 23:48:57 -0500

    Initial Commit

---
+++ TwitterGuest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterGuest
+//
+//  Created by asd on 5/28/17.
+//  Copyright © 2017 NotFound. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
